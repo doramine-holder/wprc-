476560306e49912fda06d328c06649571293b40e
dora <142666610+doramine-holder@users.noreply.github.com>
2023-08-23 14:21:50 +0300

    Create script_info.lua

---
+++ main/main/main/main/main/wprcexclusive/script_info.lua
@@ -0,0 +1,10 @@
+local info = {
+  discord = "nuh uh";
+  status = "enabled";
+  announcement = "waddup";
+  support_discord = {
+    strucid = "no one yet";
+    da_hood = "wprrc";
+  }
+}
+return info
